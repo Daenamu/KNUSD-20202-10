3fb29d27ac3c05d6b89b6db6b26e92c23cf47994
Daenamu <dheodnr318@naver.com>
2020-10-09 22:49:35 +0900

    verson1 server complete

--- team10/main/models.py
+++ team10/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='TITLE', max_length=100)
+    slug = models.SlugField('SLUG', allow_unicode=True,
+                            help_text='calender short title')
+    content = models.TextField('Content', blank=True)
+    upload_dt = models.DateTimeField('Upload Date')
+    department = models.CharField('Department', max_length=50)
+    url = models.URLField('URL', unique=True)
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        db_table = 'main_post'
+        ordering = ('-upload_dt', )
+
+    def __str__(self):
+        return self.title
--- team10/main/urls.py
+++ team10/main/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from main import views
 
 app_name = 'main'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.PostLV.as_view(), name='index'),
+    path('post/', views.PostLV.as_view(), name='post_list'),
+    path('post/<int:pk>/', views.PostDV.as_view(), name='post_detail'),
+]
