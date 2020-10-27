a7ad86971c655ee0d794e5b68cf247eb31cbab7c
Daenamu <dheodnr318@naver.com>
2020-10-27 23:14:35 +0900

    이전글, 다음글 기능 구현

--- team10/main/models.py
+++ team10/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -20,6 +21,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('main:post_detail', args=(self.id, ))
 
     def get_previous(self):
         return self.get_previous_by_upload_dt()
