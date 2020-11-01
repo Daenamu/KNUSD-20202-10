e8bf7ba2ef80febec22f417580a1dc514448231d
Daenamu <dheodnr318@naver.com>
2020-11-01 20:10:07 +0900

    search 기능

--- team10/main/views.py
+++ team10/main/views.py
@@ -10,6 +10,12 @@ class PostLV(ListView):
     model = Post
     paginate_by = 20
 
+    def get_queryset(self):
+        try:
+            search_key = self.request.GET['search_key']
+            return Post.objects.filter(title__icontains=search_key)
+        except:
+            return Post.objects.all()
 
 class PostDV(DetailView):
     template_name = 'main/post_detail.html'
