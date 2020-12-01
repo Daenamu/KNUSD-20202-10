d99727a0cb03ff19720dfa5e311580e0a8685234
KimMinyeong09 <minyeong9070@naver.com>
2020-12-02 02:54:48 +0900

    Create 의류학과

--- team10/crawler/CrawlTest.py
+++ team10/crawler/CrawlTest.py
@@ -1,5 +1,6 @@
 from haksaCrawl import extract_indeed_pages, extract_indeed_notices
 from childCrawl import child_extract_indeed_pages, child_extract_indeed_notices
+from fashionCrawl import fashion_extract_indeed_pages, fashion_extract_indeed_notices
 
 '''crawl 테스트용'''
 
@@ -8,5 +9,9 @@ from childCrawl import child_extract_indeed_pages, child_extract_indeed_notices
 #indeed_jobs = extract_indeed_notices(last_indeed_page)
 
 #아동학부 crawl test
-pages = child_extract_indeed_pages()
-child_extract_indeed_notices(pages)
+#pages = child_extract_indeed_pages()
+#child_extract_indeed_notices(pages)
+
+#의류학과 crawl test
+pages = fashion_extract_indeed_pages()
+fashion_extract_indeed_notices(pages)
