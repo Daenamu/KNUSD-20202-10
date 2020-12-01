8d5e7be44bc61acb8afb0735e4b470816dd24a17
KimMinyeong09 <minyeong9070@naver.com>
2020-12-02 01:26:06 +0900

    아동학부 수정 및 치대 삭제

--- team10/crawler/CrawlTest.py
+++ team10/crawler/CrawlTest.py
@@ -1,5 +1,4 @@
 from haksaCrawl import extract_indeed_pages, extract_indeed_notices
-from dentistryCrawl import dent_extract_indeed_pages, dent_extract_indeed_notices
 from childCrawl import child_extract_indeed_pages, child_extract_indeed_notices
 
 '''crawl 테스트용'''
@@ -7,10 +6,6 @@ from childCrawl import child_extract_indeed_pages, child_extract_indeed_notices
 #학사공지 crawl test
 #last_indeed_page = extract_indeed_pages()
 #indeed_jobs = extract_indeed_notices(last_indeed_page)
-
-#치과대학 crawl test
-#last = dent_extract_indeed_pages()
-#dent_extract_indeed_notices(last)
 
 #아동학부 crawl test
 pages = child_extract_indeed_pages()
